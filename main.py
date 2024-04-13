e10f4c0e7b11e6573e4658bbcbb291ac9c3479a1
Dimitri WALTER <dimitri.walter@ens.uvsq.fr>
2024-04-13 11:30:38 +0200

    begin

---
+++ main.py
@@ -0,0 +1,19 @@
+import time
+
+
+class Source(object):
+    def __init__(self, id, parametre,):
+        self.source_id = id
+
+    def generate_paquet(self):
+        time.sleep()
+
+
+class Buffer(object):
+    def __init__(self):
+        pass
+
+
+class Paquet(object):
+    def __init__(self):
+        pass
