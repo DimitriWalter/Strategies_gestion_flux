121bb41abc1cb42ae16f2e497161cadae2a71117
Dimitri WALTER <dimitri.walter@ens.uvsq.fr>
2024-04-21 21:10:47 +0200

    update

--- essai.py
+++ essai.py
@@ -4,16 +4,17 @@ import random as rd
 
 class Source(object):
     id = 0  
-    def __init__(self, lambd, time):
+    def __init__(self, lambd, time, app):
         Source.id += 1  
         self.id = Source.id  
         self.paquets = list()  
         self.taux_arrive = lambd
         self.time = time
+        self.app = app
 
     def generer_paquet(self):  
         paquets = list()
-        for _ in range(5):
+        for _ in range(10):
             contenu = list()
             for i in range(rd.randint(2, 9)):
                 contenu.append("")
@@ -24,44 +25,36 @@ class Source(object):
 
         return paquets
     
-    def arrive(self, paquet, buffer):  
+    def arrive(self, paquet, buffer, app):  
         if paquet.taux_arrive > Buffer.taux_lien:
             t = rd.uniform(0, self.time/self.taux_arrive)
             time.sleep(t)
-            buffer + paquet
+            buffer.insert(paquet, app)
         else:
             buffer.paquets_transmis.append(paquet)
 
 class Buffer(object):
     id = 0  
     taux_lien = 0.4  
-    def __init__(self, capacite):
+    def __init__(self, capacite, app):
         Buffer.id += 1
         self.id = Buffer.id
         self.capacite = capacite  
         self.queue = list()  
         self.paquets_perdus = list()  
-        self.paquets_transmis = list()  
+        self.paquets_transmis = list()
+        self.app = app
 
-    def __add__(self, paquet):  
+    def insert(self, paquet):
         if len(self.queue) < self.capacite:
             self.queue.append(paquet)
-            app.buffer_text.insert(tk.END, f"Paquet {paquet.id} arrivé dans le buffer\n")
-            app.buffer_text.see(tk.END)
         else:
             self.paquets_perdus.append(paquet)
-            app.buffer_text.insert(tk.END, f"Paquet {paquet.id} est perdu\n")
-        app.buffer_text.see(tk.END)
 
     def retrait(self):  
         if self.queue:
             paquet = self.queue.pop(0)
-            app.buffer_text.insert(tk.END, f"Paquet {paquet.id} est sorti du buffer\n")
-            time.sleep(1)
             self.paquets_transmis.append(paquet)
-            app.buffer_text.insert(tk.END, f"Paquet {paquet.id} est transmis sur le lien\n")
-            time.sleep(1)
-        app.buffer_text.see(tk.END)
 
 
 class Paquet(object):
@@ -78,31 +71,48 @@ class App(tk.Tk):
     def __init__(self):
         super().__init__()
         self.title("Gestion de flux")
-        self.geometry("1000x700")
+        self.geometry("880x790")
         self.geometry("+380+160")
+        self.configure(bg="#D5CCDC")
 
-        self.source = Source(1, 0.5)
-        self.buffer = Buffer(47)
+        self.source = Source(2, 1, self)
+        self.buffer = Buffer(47, self)
 
-        self.buffer_label = tk.Label(self, text="Buffer", font="bold")
-        self.buffer_label.grid(row=0, column=0)
+        self.widgets()
+        self.after(10, self.actualise)
 
-        self.buffer_text = tk.Text(self, height=38, width=50)
-        self.buffer_text.grid(row=1, column=0)
+    
+    def widgets(self):
+
+        self.buffer_label = tk.Label(self, text=f"Buffer", font=("Helvetica", 15, "bold"), fg="white", bg="blue", padx=45, pady=10)
+        self.buffer_label.place(x=150, y=7)
+
+        self.buffer_canvas = tk.Canvas(self,height=600, width=400, bg="white")
+        self.buffer_canvas.place(x=20, y=60)
         
-        self.lien_text = tk.Text(self, height=38, width=50)
-        self.lien_text.grid(row=1, column=1)
+        self.lien_label = tk.Label(self, text="Lien", font=("Helvetica", 15, "bold"), fg="white", bg="green", padx=45, pady=10)
+        self.lien_label.place(x=580,y=7)
 
-        self.arrival_button = tk.Button(self, text="Arrivée Paquet", command=self.arrive_paquet)
-        self.arrival_button.grid(columnspan=2)
+        self.lien_canvas = tk.Canvas(self, height=600, width=400, bg="white")
+        self.lien_canvas.place(x=450, y=60)
 
-        self.retrait_button = tk.Button(self, text="Retrait Paquet", command=self.buffer.retrait)
-        self.retrait_button.grid()
+        self.paquets_perdus = tk.Label(self, text=f"Nombres de paquets perdus: {len(self.buffer.queue)}", font=("Helvetica", 13, "bold"), fg="white", bg="red", padx=45, pady=10)
+        self.paquets_perdus.place(x=250, y=682)
 
-    def arrive_paquet(self):
+        self.taux_perdus = tk.Label(self, text=f"Taux de paquets perdus: {len(self.buffer.queue)}", font=("Helvetica", 13, "bold"), fg="white", bg="red", padx=45, pady=10)
+        self.taux_perdus.place(x=267, y=733)
+
+    def actualise(self):
+        pass
+    
+    def programme(self):
         paquets = self.source.generer_paquet()
         for paquet in paquets:
-            self.source.arrive(paquet, self.buffer)
+            self.source.arrive(paquet, self.buffer, self)
 
-app = App()
-app.mainloop()
+
+
+
+if __name__ == "__main__":
+    app = App()
+    app.mainloop()
